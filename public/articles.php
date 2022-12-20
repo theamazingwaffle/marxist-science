eae532ced09b1d33c890219f47c61d0bb2610455
Morozov Pavel <filipencop@mail.ru>
2022-12-20 19:20:44 +0200

    Changed stuff ig 2

---
+++ public/articles.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <title>Наука Марксизм</title>
+  </head>
+  <body>
+    <nav class="main-navbar">
+      <div class="navbar-inner">
+        <a href="#" class="logo">
+          <img src="img/nm-logo.png">
+        </a>
+        <ul class="navbar-menu">
+          <li><a href="/articles.php">Статьи</a></li>
+          <li><a href="/circles.php">Кружки</a></li>
+          <li><a href="/archive.php">Архив</a></li>
+        </ul>
+        <form id="search">
+          <input type="text" placeholder="Поиск">
+        </form>
+        <!-- <a href="#"><span class="material-symbols-outlined menu">menu</span></a> -->
+      </div>
+    </nav>
+    <div class="content">
+      <section class="about">
+        <h1 class="heading">
+          Статьи
+        </h1>
+        <p>
+          Здесь будет список статей НМ.
+        </p>
+      </section>
+      <footer>
+        Copyright &copy; 2022 Marxist Science | All rights reserved.
+      </footer>
+    </div>
+  </body>
+</html>
