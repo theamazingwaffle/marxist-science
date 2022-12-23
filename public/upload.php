5e5407d896aca678d527a56fe039483f8b5e49c9
Morozov Pavel <filipencop@mail.ru>
2022-12-23 23:28:50 +0200

    Cleaned up

--- public/upload.php
+++ public/upload.php
@@ -1,22 +1,27 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
-// TODO: Error handling and setting the correct request headers, depending on
-// the context.
+$data = null;
 
 if ($_SERVER["REQUEST_METHOD"] === 'POST') {
-    $target_dir = "uploads/";
-    $file = $_FILES["fileToUpload"];
-    $splitName = explode('.', $file["name"]);
-    $newFileName = uniqid('', false) . '.' . end($splitName);
-    $target_file = $target_dir . basename($newFileName);
+    if (!isset($_FILES["fileToUpload"]) || $_FILES["fileToUpload"]["size"] === 0) {
+        http_response_code(400); // Bad Request
+    } else {
+        $target_dir = "uploads/";
+        $file = $_FILES["fileToUpload"];
+        $splitName = explode('.', $file["name"]);
+        $newFileName = uniqid('', false) . '.' . end($splitName);
+        $target_file = $target_dir . basename($newFileName);
 
-    $move_result = move_uploaded_file($file["tmp_name"], $target_file);
+        $move_result = move_uploaded_file($file["tmp_name"], $target_file);
 
-    $data = array(
-        "content-url" => "/" . $target_file,
-    );
-
-    echo json_encode($data);
+        $data = array(
+            "content-url" => "/" . $target_file,
+            "file" => $file,
+        );
+    }
 }
+
+echo json_encode($data);
+
 ?>
