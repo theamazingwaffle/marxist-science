01ea2c152d8c10c642a719656a6b014cab2bb862
Morozov Pavel <filipencop@mail.ru>
2022-12-23 21:17:10 +0200

    minor tweak and cleaning up

--- public/upload.php
+++ public/upload.php
@@ -1,6 +1,5 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-$ok = true;
 
 // TODO: Error handling and setting the correct request headers, depending on
 // the context.
@@ -8,16 +7,16 @@ $ok = true;
 if ($_SERVER["REQUEST_METHOD"] === 'POST') {
     $target_dir = "uploads/";
     $file = $_FILES["fileToUpload"];
-    $target_file = $target_dir . basename($file["name"]);
-    if (file_exists($target_file)) {
-        $ok = false;
-    }
+    $splitName = explode('.', $file["name"]);
+    $newFileName = uniqid('', false) . '.' . end($splitName);
+    $target_file = $target_dir . basename($newFileName);
 
-    move_uploaded_file($file["tmp_name"], $target_file);
+    $move_result = move_uploaded_file($file["tmp_name"], $target_file);
 
     $data = array(
-        "uploaded-content-url" => "/" . $target_file
+        "content-url" => "/" . $target_file,
     );
+
     echo json_encode($data);
 }
 ?>
