b499a7e125068aae2b322873e06241400d0c2c38
Morozov Pavel <filipencop@mail.ru>
2022-12-30 00:58:05 +0200

    Completed error handling

--- public/upload.php
+++ public/upload.php
@@ -1,26 +1,37 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
+// DON'T OUTPUT ANYTHING HERE
+if ($_SERVER["REQUEST_METHOD"] !== 'POST') {
+    http_response_code(400);
+} else {
+    $data = array();
+    $target_dir = "uploads/";
 
-$data = null;
-
-if ($_SERVER["REQUEST_METHOD"] === 'POST') {
     if (!isset($_FILES["fileToUpload"]) || $_FILES["fileToUpload"]["size"] === 0) {
         http_response_code(400); // Bad Request
+        $data["error"] = "No valid file provided";
     } else {
-        $target_dir = "uploads/";
         $file = $_FILES["fileToUpload"];
-        $splitName = explode('.', $file["name"]);
-        $newFileName = uniqid('', false) . '.' . end($splitName);
-        $target_file = $target_dir . basename($newFileName);
+        $splitName = explode('.', basename($file["name"]));
+        $extension = end($splitName);
 
-        $move_result = move_uploaded_file($file["tmp_name"], $target_file);
-
-        $data = array(
-            "content-url" => "/" . $target_file,
-        );
+        if (strlen($extension) == 0) {
+            http_response_code(400);
+            $data["error"] = "Invalid file name or extension";
+        } else {
+            $newFileName = uniqid('', false).'.'.$extension;
+            $target_file = $target_dir . $newFileName;
+            $move_result = move_uploaded_file($file["tmp_name"], $target_file);
+            if (!$move_result) {
+                http_response_code(500); // Internal server error
+                $data["error"] = "Failed to upload file";
+            } else {
+                $data["content-url"] = "/".$target_file;
+            }
+        }
     }
+
+    header('Content-Type: application/json; charset=utf-8');
+    // OUTPUT HERE
+    echo json_encode($data);
 }
-
-echo json_encode($data);
-
 ?>
